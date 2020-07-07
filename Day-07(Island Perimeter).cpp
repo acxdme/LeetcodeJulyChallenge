1465c8e9da176f97773d8422c74fa636d247a34d
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-07-07 23:19:29 +0530

    Update Day-07(Island Perimeter).cpp

--- Day-07(Island Perimeter).cpp
+++ Day-07(Island Perimeter).cpp
@@ -2,7 +2,23 @@
 Time complexity : O(n^2)
 Space complexity : O(1)
 My approach : Traverse the given 2d matrix and for every 1 found,check which of the following sides are 1 also (i.e top,bottom,left and right).
+
+Question: 
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ Example:
+
+Input:
+[[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+
+Output: 16
 */
+
 class Solution {
 public:
     
