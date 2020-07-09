db68e3369104fa223acc55e6b7bfaccb36fd0ad6
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-07-09 21:40:04 +0530

    Create Day-08(2 sum).cpp

---
+++ Day-08(2 sum).cpp
@@ -0,0 +1,29 @@
+/*
+ This is a must do question!
+ Time complexity : O(n^2)
+ Space complexity : O(1)
+ My approach : use two pointer method , after fixing an element in the array. 
+ */
+ 
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> res;
+    for (unsigned int i=0; i<nums.size(); i++) {
+        if ((i>0) && (nums[i]==nums[i-1]))
+            continue;
+        int l = i+1, r = nums.size()-1;
+        while (l<r) {
+            int s = nums[i]+nums[l]+nums[r];
+            if (s>0) r--;
+            else if (s<0) l++;
+            else {
+                res.push_back(vector<int> {nums[i], nums[l], nums[r]});
+                while (nums[l]==nums[l+1]) l++;
+                while (nums[r]==nums[r-1]) r--;
+                l++; r--;
+            }
+        }
+    }
+    return res;
+}
