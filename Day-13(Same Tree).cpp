95696f494391b39d7fa56bbf57f0932e16a434f6
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-07-13 17:47:30 +0530

    Create Day-13(Same Tree).cpp

---
+++ Day-13(Same Tree).cpp
@@ -0,0 +1,46 @@
+/*
+Time complexity : O(n)
+Space complexity :O(1)
+My approach : Traverse both the trees simultaneously and check for the node-> val and also keep an eye on when one tree terminates faster than the other.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   bool flag=true;
+   void preorder(TreeNode* a, TreeNode* b)
+   {
+       if(a==NULL and b==NULL)
+           return;
+       if((a==NULL and b!=NULL) or(b==NULL and a!=NULL))
+       {
+           flag=false;
+           return;
+       }
+       if(a->val!=b->val)
+       {
+           flag=false;
+           return;
+       }
+       
+       preorder(a->left,b->left);
+       preorder(a->right,b->right);
+       
+     return;  
+   }
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        preorder(p,q);
+        return flag ;
+        
+    }
+};
