5c824933b6e292004386c20ad7ad6ac68f68172c
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-07-15 10:45:41 +0530

    Update Day-14(Angle Between Hands of a Clock).cpp

--- Day-14(Angle Between Hands of a Clock).cpp
+++ Day-14(Angle Between Hands of a Clock).cpp
@@ -1,6 +1,18 @@
 /*
-Time complexity :
-Space complexity :
-My approach :
+Time complexity : O(1)
+Space complexity : O(1)
+My approach : the hour hand makes an angle of 30⁰ in an hour where as the minute hand makes an angle of 6⁰ per minute, rest of it is maths actually.
 Question :Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
 */
+
+class Solution {
+public:
+    double angleClock(int hour, int minutes)
+    {
+        double ha = (hour +(double) minutes/60) * 30;
+        double ma =  minutes * 6;
+       // cout<<ha<<" "<<ma<<endl;
+        double t= abs(ma - ha);
+         return min(t,360-t);
+    }
+};
